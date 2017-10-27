92d3ec4ea452ffae1e42cd2089b7c4d0bf538c7e
Marco De Jesus <marcoantoniod@herbalife.com>
2017-10-27 06:39:49 -0500

    Automation with npm scripts, Transpiling with Babel configured in the starter kit by using ES6

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,10 +1,10 @@
-var express = require('express');
-var path = require('path');
-var open = require('open');
+import express from 'express';
+import path from 'path';
+import open from 'open';
 
-var  port = 3001;
+const port = 3001;
 
-var app = express();
+const app = express();
 
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, '../src/index.html'));
@@ -16,5 +16,4 @@ app.listen(port, function(err){
   }else{
     open('http://localhost:' + port);
   }
-
 });
---
+++ buildScripts/startMessage.js
@@ -0,0 +1,6 @@
+//ES5 syntax var chalk = require('chalk');
+
+// ES6 code
+import chalk from 'chalk';
+
+console.log(chalk.green('Starting app in dev mode...'));
