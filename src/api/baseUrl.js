d88616c5e0102541cda1c03f7438243a804e89ae
Marco De Jesus <marcoantoniod@herbalife.com>
2017-10-30 12:47:19 -0600

    Switching between Production API and MockApi

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,4 +1,22 @@
 export default function getBaseUrl(){
+  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3002/' : '/';
+
+  /*
   const inDevelopment = window.location.hostname === 'localhost';
   return inDevelopment ? 'http://localhost:3002/' : '/';
+
+  */
+}
+
+
+// https://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+function getQueryStringParameterByName(name, url){
+  if(!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    results = regex.exec(url);
+  if(!results) return null;
+  if(!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+
 }
